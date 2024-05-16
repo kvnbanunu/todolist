6ed7d95eaf0052ed41d0963f941b157ce5bc4493
kvnbanunu <kvnbanunu@gmail.com>
2024-05-15 21:57:10 -0700

    Added delete functionality with a material icon.

--- todo.js
+++ todo.js
@@ -23,6 +23,19 @@ function todoMain() {
 
 		let liElem = document.createElement("li");
 		liElem.innerText = inputValue;
+
+		let spanElem = document.createElement("span");
+		spanElem.innerText = "delete";
+		spanElem.className = "material-symbols-outlined";
+
+		spanElem.addEventListener("click", deleteItem, false);
+
+		liElem.appendChild(spanElem);
+
 		ulElem.appendChild(liElem);
+
+		function deleteItem() {
+			liElem.remove();
+		}
 	}
 }
