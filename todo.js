789d7f3a1b14205847743fbba3127a89cee833b2
kvnbanunu <kvnbanunu@gmail.com>
2024-05-18 01:02:57 -0700

    Implemented task marking with strikethrough and checkboxing.

--- todo.js
+++ todo.js
@@ -15,6 +15,8 @@ function todoMain() {
 	}
 
 	function onChange(event) {
+		let flag = true;
+
 		let inputValue = inputElem.value;
 
 		// This allows html injection
@@ -22,7 +24,17 @@ function todoMain() {
 		inputElem.value = "";
 
 		let liElem = document.createElement("li");
-		liElem.innerText = inputValue;
+
+		let checkboxElem = document.createElement("input");
+		checkboxElem.type = "checkbox";
+		liElem.appendChild(checkboxElem);
+
+		let textElem = document.createElement("span");
+		textElem.innerText = inputValue;
+		liElem.appendChild(textElem);
+
+		// liElem.innerText = inputValue;
+		liElem.addEventListener("click", onClick, false);
 
 		let spanElem = document.createElement("span");
 		spanElem.innerText = "delete";
@@ -36,6 +48,19 @@ function todoMain() {
 
 		function deleteItem() {
 			liElem.remove();
+		}
+
+		function onClick() {
+			if (flag) {
+				// this.style.textDecoration = "line-through";
+				this.classList.add("strike");
+				flag = !flag;
+			} else {
+				// this.style.textDecoration = "none";
+				this.classList.remove("strike");
+				flag = !flag;
+			}
+			
 		}
 	}
 }
